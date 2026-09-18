2357acd14b78409a7c98c464acdd197724c08112
IsabelliSF <isabellisantosfirmino@gmail.com>
2026-09-18 16:29:49 -0300

    number concluida

---
+++ Boolean/boolean.js
@@ -0,0 +1,11 @@
+// ativo com true
+let idade = 16;
+let maiorDeIdade = true;
+let menorDeIdade = false;
+let = maiorDeIdade = idade>= 18;
+
+console.log(maiorDeIdade);
+console.log(menorDeIdade);
+console.log(idade);
+
+
