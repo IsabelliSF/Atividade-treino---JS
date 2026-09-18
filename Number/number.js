63ed61b220587243d6585215da45d79ea6891ed3
IsabelliSF <isabellisantosfirmino@gmail.com>
2026-09-18 14:47:39 -0300

    primeiro commit

---
+++ Number/number.js
@@ -0,0 +1,34 @@
+// 1 (soma de 2 números)
+let numero1 = 20;
+let numero2 = 75;
+
+console.log(`Resultado da subtração: ${numero1 + numero2}` );
+
+// 2 (subtração)
+let termo1 = 1400;
+let termo2 = 359;
+
+console.log(`Resultado da subtração: ${termo1 - termo2}`);
+
+// 3 (multiplicação)
+let primeiroTermo = 157;
+let segundoTermo = 25;
+
+console.log(`Resultado da multiplicação: ${primeiroTermo * segundoTermo}`);
+
+// 4 (divisão)
+let primeiroNumero = 2000;
+let segundoNumero = 20;
+
+console.log(`Resultado da divisão ${primeiroNumero / segundoNumero}`);
+
+// 5 (média)
+let nota1 = 7;
+let nota2 = 10;
+let soma = nota1 + nota2;
+let media = soma / 2;
+
+console.log(nota1);
+console.log(nota2);
+console.log(soma);
+console.log(media);
---
+++ String/string.js
@@ -0,0 +1,55 @@
+// 1 e 2 (Nome)
+let nome = "Isabelli";
+let sobrenome = "Santos Firmino";
+let nomeCompleto = nome + sobrenome
+
+console.log(`Nome: ${nome}`);
+console.log(`Sobrenome: ${sobrenome}`);
+console.log(`Nome completo: ${nomeCompleto}`);
+
+// 3 (Frase)
+let frase= "Meu nome é Isabelli e sou estudante do SESI/SENAI"
+
+console.log(frase);
+
+// 4 (Escola)
+let escola = "SESI";
+
+console. log(`Escola: ${escola}`);
+
+// 5 (Duas Strings)
+let string1 = "Ser ou não ser?";
+let string2 = "Eis a questão.";
+let frase2 = string1 + string2;
+
+console.log(string1);
+console.log(string2);
+console.log(frase2);
+
+// 6 (Boas-vindas)
+let mensagem = "Seja bem-vindo";
+
+console.log(mensagem);
+
+// 7 (Tamanho da string)
+let invasãoNoSistemaDetectada = "Já era!";
+let regra = "Mude sua senha!";
+
+console.log(invasãoNoSistemaDetectada.length);
+console.log(regra.toUpperCase());
+
+// 8 (Letras maiúsculas)
+let maiusculo = "o esforço pra lembrar é a vontade de esquecer. - los hermanos";
+console.log(maiusculo.toUpperCase());
+
+// 9 (Letras minúsculas)
+let minusculo = "PREFIRO SER ESSA METAMORFOSE AMBULANTE DO QEU TER AQUELA VELHA OPINIÃO FORMADA SOBRE TUDO. - RAUL SEIXAS";
+console.log(minusculo.toLowerCase());
+
+// 10 Nome e cidade
+let freddie = "Farrokh Bulsara, mais conhecido por seu nome artístico Freddie Mercury nasceu em ";
+let cidade = "Cidade de Pedra - Tanzânia";
+let fraseCompleta = freddie + cidade;
+
+console.log(freddie);
+console.log(cidade);
