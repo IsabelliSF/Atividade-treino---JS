d2ea67e696ede13eddc2feb80a133d640fa49771
IsabelliSF <isabellisantosfirmino@gmail.com>
2026-09-23 15:07:58 -0300

    atualizado

--- Boolean/boolean.js
+++ Boolean/boolean.js
@@ -1,11 +1,33 @@
-// ativo com true
-let idade = 16;
+// 1 (ativo com true)
+let idade = 20;
 let maiorDeIdade = true;
-let menorDeIdade = false;
+let menorDeIdade = idade<= 17;
 let = maiorDeIdade = idade>= 18;
 
-console.log(maiorDeIdade);
-console.log(menorDeIdade);
-console.log(idade);
+console.log(`Maior de idade: ${maiorDeIdade}`);
+console.log(`Menor de idade: ${menorDeIdade}`);
+console.log(`Idade: ${idade}`);
 
+// 2 (número maior que 10)
+let number = 7;
+let maiorQueDez = true;
+let = maiorQueDez = number >= 11;
+let menorQueDez = number <= 9;
+
+
+console.log(`Número: ${number}`);
+console.log(`Maior que 10: ${maiorQueDez}`);
+console.log(`Menor que 10: ${menorQueDez}`);
+
+// 3 (idade maior ou menor que 18)
+let idade2 = 21;
+let maiorQue18 = true;
+let menorQue18 = number <=17;
+let = maiorQue18 = number >=18;
+let igual18 = idade = 18;
+
+console.log(`Idade: ${idade}`);
+console.log(`Maior que 18: ${maiorQue18}`);
+console.log(`Menor que 18: ${menorQue18}`);
+console.log(`Igual a 18: ${idade=18}`);
 
